29b16720cf1efa52804e98ebdf1bd42885d364b9
adoncheva <annadoncheva@yahoo.com>
2015-10-01 10:35:02 +0300

    Seventh Problem

---
+++ 2.07. PrintFirstAndLast Name/PrintFirstAndLastName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+//Problem 7.	Print First and Last Name
+//Create console application that prints your first and last name, each at a separate line.
+
+
+
+    class PrintFirstAndLastName
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First Name:Faith"); 
+            Console.WriteLine("Last Name:Praise"); 
+        }   
+
+    }
+
